d8de166f6b83e31ed32491c3950d13efb1cbfc90
laibahussain <laibah42@gmail.com>
2024-11-04 20:47:29 +0500

    React (first lec)

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+const App = ()=>{
+  return(
+    <div>
+      <h2 className="bg-pink-300 text-3xl text-center text-red-600">First Lecture of React.jsx in Tailwind.css</h2>
+    </div>
+  )
+}
+export default App 
